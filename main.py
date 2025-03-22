91c314f3be3c091e21e333638d2ad56ff34788bd
LucaDC913 <lucadc_913@outlook.com>
2025-03-22 10:24:49 -0300

    First Upload
    First upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# Programa para sumar dos números
+
+# Solicitar al usuario que ingrese los números
+numero1 = float(input("Ingresa el primer número: "))
+numero2 = float(input("Ingresa el segundo número: "))
+
+# Sumar los números
+resultado = numero1 + numero2
+
+# Mostrar el resultado
+print(f"La suma de {numero1} y {numero2} es: {resultado}")
